5ac931fc208a22fada3be3b393d2d5ef7ff01f64
nguyenhongson <nguyenhongson20433@gmail.com>
2023-06-03 15:35:18 +0700

    feature/DT-13-GetAllProduct

--- index.js
+++ index.js
@@ -3,12 +3,14 @@ import morgan from "morgan";
 import * as dotenv from "dotenv";
 import mongoose from "mongoose";
 import categoryRoute from "./src/routes/categoryRoute.js";
+import productRoute from "./src/routes/productRoute.js";
 dotenv.config();
 const app = express();
 const port = process.env.PORT;
 app.use(express.json());
 app.use(morgan("tiny"));
 app.use(categoryRoute);
+app.use(productRoute);
 mongoose
   .connect(
     "mongodb+srv://nguyenhongson20433:9gjfX7VDs7Z9XzK1@cluster0.cnb4wcl.mongodb.net/?retryWrites=true&w=majority"
---
+++ src/models/productModel.js
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+import mongoosePaginate from "mongoose-paginate-v2";
+const productSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+    },
+    image: {
+      type: String,
+    },
+    price: {
+      type: Number,
+    },
+    description: {
+      type: String,
+    },
+    categoryId: {
+      type: mongoose.Types.ObjectId,
+      ref: "Categories",
+      require: true,
+    },
+  },
+  { timestamps: true, versionKey: false }
+);
+productSchema.plugin(mongoosePaginate);
+export default mongoose.model("Products", productSchema);
