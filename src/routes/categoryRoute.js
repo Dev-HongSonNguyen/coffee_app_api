8fca34a4923663678c5e7400c8d963ba5993e5a2
nguyenhongson <nguyenhongson20433@gmail.com>
2023-06-03 10:08:50 +0700

    feature/DT-7-ListOneCategory

--- src/routes/categoryRoute.js
+++ src/routes/categoryRoute.js
@@ -1,5 +1,9 @@
 import express from "express";
-import { getAllCategory } from "../controllers/categoryController.js";
+import {
+  getAllCategory,
+  getOneCategory,
+} from "../controllers/categoryController.js";
 const router = express.Router();
 router.get("/categories", getAllCategory);
+router.get("/categories/:id", getOneCategory);
 export default router;
