a8739aafd68bb396dd71e2e4c9cdd7cbd5860dd1
Duy-Hiep <106466621+Duy-Hiep@users.noreply.github.com>
2023-06-04 00:44:12 +0700

    feature/DT-16-UpdateProduct

--- src/routes/productRoute.js
+++ src/routes/productRoute.js
@@ -1,8 +1,9 @@
 import express from "express";
-import { createProduct, deleteProduct, getAllProduct, getOneProduct } from "../controllers/productController.js";
+import { createProduct, deleteProduct, getAllProduct, getOneProduct, updateProduct } from "../controllers/productController.js";
 const router = express.Router();
 router.get("/products", getAllProduct);
 router.get("/products/:id", getOneProduct);
 router.post("/products", createProduct);
+router.put("/products/:id", updateProduct);
 router.delete("/products/:id", deleteProduct);
 export default router;
