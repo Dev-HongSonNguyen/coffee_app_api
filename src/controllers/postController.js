8ed404a8b43df5b7d0d87bb68f26a147428ce521
heyIamThoong <heyiamthoong@gmail.com>
2023-06-07 16:12:47 +0700

    feature/DT-41-AddPost

--- src/controllers/postController.js
+++ src/controllers/postController.js
@@ -1,64 +1,88 @@
 import postModel from "../models/postModel.js";
 import validatePost from "../schemas/postValidate.js";
-export const getOnePost = async(req, res) => {
+export const getOnePost = async (req, res) => {
   try {
-      const post = await postModel.findById(req.params.id)  
-      if(!post){
-          return res.json({
-              message: "Lấy tài nguyên thất bại !",
-          });
-      }
+    const post = await postModel.findById(req.params.id)
+    if (!post) {
       return res.json({
-          message: "Lấy tài nguyên thành công !",
-          post,
-      })
-  } catch (error) {
-      return res.status(400).json({
-          message: error,
+        message: "Lấy tài nguyên thất bại !",
       });
+    }
+    return res.json({
+      message: "Lấy tài nguyên thành công !",
+      post,
+    })
+  } catch (error) {
+    return res.status(400).json({
+      message: error,
+    });
   }
 };
 export const getAllPost = async (req, res) => {
-    try {
-      const post = await postModel.find()
-      if (!post) {
-        return res.status(404).json({
-          message: "Lấy tài nguyên thất bại !",
-        });
-      }
-      return res.json({
-        message: "Lấy tài nguyên thành công !",
-        post,
+  try {
+    const post = await postModel.find()
+    if (!post) {
+      return res.status(404).json({
+        message: "Lấy tài nguyên thất bại !",
       });
-    } catch (error) {
+    }
+    return res.json({
+      message: "Lấy tài nguyên thành công !",
+      post,
+    });
+  } catch (error) {
+    return res.status(400).json({
+      message: error.message,
+    });
+  }
+};
+export const createPost = async (req, res) => {
+  try {
+    const { error } = validatePost.validate(req.body);
+    if (error) {
       return res.status(400).json({
-        message: error.message,
+        message: error.details[0].message,
       });
     }
-  };
-export const updatePost = async(req, res) => {
-    
-    try {
-        const {error} = validatePost.validate(req.body);
-        if(error){
-            return res.status(400).json({
-                massage: error.details[0].message,
-            })
-        }
-        const post = await postModel.findByIdAndUpdate({_id: req.params.id}, req.body, {new: true})
-        if(!post){
-            return res.json({
-                message: "Cập nhật tài nguyên không thành công !"
-            })
-        }
-        return res.json({
-            message: "Cập nhật tài nguyên thành công !",
-            post,
-        })
-    } catch (error) {
-        return res.json(400).json({
-            message: error,
-        })
+    const post = await postModel.create(req.body);
+    if (!post) {
+      return res.json({
+        message: "Thêm tài nguyên không thành công !",
+      });
     }
-  };
+    return res.json({
+      message: "Thêm tài nguyên thành công !",
+      post,
+    });
+  } catch (error) {
+    return res.status(400).json({
+      message: error,
+    });
+  }
+};
+export const updatePost = async (req, res) => {
+
+  try {
+    const { error } = validatePost.validate(req.body);
+    if (error) {
+      return res.status(400).json({
+        massage: error.details[0].message,
+      })
+    }
+    const post = await postModel.findByIdAndUpdate({ _id: req.params.id }, req.body, { new: true })
+    if (!post) {
+      return res.json({
+        message: "Cập nhật tài nguyên không thành công !"
+      })
+    }
+    return res.json({
+      message: "Cập nhật tài nguyên thành công !",
+      post,
+    })
+  } catch (error) {
+    return res.json(400).json({
+      message: error,
+    })
+  }
+};
 
--- src/routes/postRouter.js
+++ src/routes/postRouter.js
@@ -1,8 +1,9 @@
 import express from "express";
-import { getAllPost, getOnePost, updatePost } from "../controllers/postController.js";
+import { createPost, getAllPost, getOnePost, updatePost } from "../controllers/postController.js";
 
 const router = express.Router();
 router.get("/post/:id", getOnePost);
 router.get("/post", getAllPost);
+router.post("/post", createPost);
 router.put("/post/:id", updatePost);
 export default router;
